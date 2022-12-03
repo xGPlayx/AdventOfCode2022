0eabf0117c0970a2490dae0813cc9b59a14e2446
xGPlayx <71895391+xGPlayx@users.noreply.github.com>
2022-12-03 06:52:15 +0100

    Add files via upload

---
+++ AoC3/main.py
@@ -0,0 +1,29 @@
+liste = open('cal.txt', 'r')
+arr2 = []
+arr1 = []
+setOf3 = []
+
+for i in liste.readlines():
+    if len(setOf3) == 2: #Aufgabe 2
+        for j in i:
+            if j in setOf3[0] and j in setOf3[1]:
+                doubles.append(j)
+                setOf3 = []
+                break;
+    else:
+        setOf3.append(i)
+    for j in i[0:len(i) // 2]: #Aufgabe 1
+        if j in i[len(i) // 2:]:
+            arr1.append(j)
+            break;
+
+def getSum(arr): #
+    sum = 0
+    for k in arr:
+        if ord(k) > 90:
+            sum += ord(k) - 96
+        else:
+            sum += ord(k) - 64 + 26
+    return sum
+print(getSum(doubles))
+print(getSum(arr1))
